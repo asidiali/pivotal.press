1af78b13725ff610f5c8f8397ac22ec6d7c70cdb
Adam Sidiali <adamsidiali@gmail.com>
2016-10-30 10:12:50 -0700

    wip activity projects

--- src/views/ProjectsView/index.js
+++ src/views/ProjectsView/index.js
@@ -11,6 +11,7 @@ export default class ProjectsView extends React.Component {
 
   state = {
     projects_fetched: false,
+    projects_activity_fetched: false,
   };
 
   componentWillMount() {
@@ -19,28 +20,48 @@ export default class ProjectsView extends React.Component {
   }
 
   componentDidMount() {
-    const headers = new Headers();
-    headers.append('X-TrackerToken', ls('pp-api'));
-    fetch('https://www.pivotaltracker.com/services/v5/projects', {
-      mode: 'cors',
-      headers,
-      method: 'GET',
-    }).then(res => res.json()).then((res) => {
-      ls.set('pp-projects', res);
-      this.setState({projects_fetched: true});
-    });
+    if (ls('pp-api') && ls('pp-me')) {
+      const headers = new Headers();
+      headers.append('X-TrackerToken', ls('pp-api'));
+      fetch('https://www.pivotaltracker.com/services/v5/projects', {
+        mode: 'cors',
+        headers,
+        method: 'GET',
+      }).then(res => res.json()).then((res) => {
+        ls.set('pp-projects', res);
+        this.setState({projects_fetched: true});
+
+        // fetch activity for each project
+        res.forEach((project) => {
+          fetch(`https://www.pivotaltracker.com/services/v5/projects/${project.id}/activity?limit=5`, {
+            mode: 'cors',
+            headers,
+            method: 'GET',
+          }).then(response => response.json()).then((activityRes) => {
+            ls.set(`pp-project-${project.id}-activity`, activityRes);
+          });
+        });
+
+        this.setState({projects_activity_fetched: true});
+      });
+    }
   }
 
   render() {
     return (
       <div style={styles.base}>
         <div style={styles.projectsWrapper}>
-          {this.state.projects_fetched ? ls('pp-projects').map((project, projectIndex) => (
+          {(this.state.projects_fetched && this.state.projects_activity_fetched) ? ls('pp-projects').map((project, projectIndex) => (
             <div key={projectIndex} style={styles.projectCard} onClick={() => {
               ls.set(`pp-project-${project.id}-details`, project);
               hashHistory.push(`/projects/${project.id}`)
             }}>
               <span style={styles.projectName}>{project.name}</span>
+              <ul style={styles.projectActivityList}>
+                {ls(`pp-project-${project.id}-activity`).length ? ls(`pp-project-${project.id}-activity`).map((activity) => (
+                  <li style={styles.projectActivityListItem}>{activity.message}</li>
+                )) : false}
+              </ul>
             </div>
           )) : (
             <Loader />
--- src/views/ProjectsView/styles.js
+++ src/views/ProjectsView/styles.js
@@ -15,15 +15,13 @@ export default {
     margin: '0.5%',
     display: 'flex',
     flexFlow: 'column nowrap',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     padding: 20,
     boxSizing: 'border-box',
     borderTop: '0px solid #ddd',
     borderRight: '1px solid #ddd',
     borderLeft: '1px solid #ddd',
     borderBottom: '2px solid #ddd',
-    fontWeight: 700,
-    fontSize: '1.2em',
     overflow: 'hidden',
     cursor: 'pointer',
     '@media (max-width: 1440px)': {
@@ -36,5 +34,24 @@ export default {
   projectName: {
     textOverflow: 'ellipsis',
     whiteSpace: 'nowrap',
+    fontWeight: 700,
+    fontSize: '1.2em',
+    margin: '10px 0',
+    paddingBottom: 10,
+    borderBottom: '1px solid #eee',
   },
+
+  projectActivityList: {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+    display: 'flex',
+    flexFlow: 'column nowrap',
+  },
+  projectActivityListItem: {
+    color: '#aaa',
+    margin: '2px 0',
+    flexFlow: '0 0 auto',
+    fontSize: '0.9em',
+  }
 };
