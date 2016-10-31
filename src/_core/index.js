4000ac8e4d4dcab8ee3f800eebf4b72ffe754f9c
Adam Sidiali <adamsidiali@gmail.com>
2016-10-31 10:14:08 -0700

    analytics

--- src/_core/index.js
+++ src/_core/index.js
@@ -43,3 +43,11 @@ if (loadedStates.includes(document.readyState) && document.body) {
 } else {
   document.addEventListener('DOMContentLoaded', main, false);
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-86630001-1', 'auto');
+ga('send', 'pageview');
